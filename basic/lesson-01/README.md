d2827dd8da97e1bcf85d7fe9065de20988770141
mike.rustia <mike.rustia.jw@gmail.com>
2020-07-05 22:03:20 +0800

    Initial commit

---
+++ basic/lesson-01/README.md
@@ -0,0 +1,14 @@
+# Lesson-01
+
+### Goal
+
+* To understand that to run a java program, it needs a class with a `main` method with this signature 
+````java
+public static void main(String[] args)
+````
+* The name of the class should be the same as the file name
+* The file should have extension of `.java`
+* To understand the java compiler
+* To understand a java package
+* To understand importing classes
+* To understand comments, java doc and inline.
---
+++ basic/lesson-01/main-class/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * This is Main.java containing the Main class and the main method.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // This will output Hello World! in console
+        System.out.println("Hello World!");
+
+        //Scanner scanner = new Scanner(System.in);
+        //String input = scanner.nextLine();
+        //System.out.println("Scanner got input of : " + input);
+    }
+}
